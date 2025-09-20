4af4cc4608027720b29bd297545d120c8a1c40b6
Marco Morandi <marcomorandi3005@gmail.com>
2025-09-20 12:52:57 +0200

    Add getComputerChoice() function to script.js
    The function returns the choice of the computer by generating a random
    number between 0 and 2 and associating each number with a possible
    choice.
    The random choice is generated via Math.random() method rounded down
    using Math.floor()

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+// function to get random computer choice
+function getComputerChoice() {
+    let random_choice = Math.floor(Math.random() * 3);
+    console.log(random_choice);
+
+    // return string based on the random number generated
+    switch(random_choice) {
+        case 0: 
+            return "rock";
+            break;
+        case 1: 
+            return "paper";
+            break;
+        case 2: 
+            return "scissors";
+            break;
+    }
+}
+
+let computer_choice = getComputerChoice();
+console.log(computer_choice);
