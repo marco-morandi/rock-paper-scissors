573a049cda225128c4696ebbf7ff76e6b2a2e34d
Marco Morandi <marcomorandi3005@gmail.com>
2025-09-21 17:28:30 +0200

    Add playRound() function
    The function simulates a round of the game, printing in the console the
    result of the round and incrementing score values.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // function to get random computer choice
 function getComputerChoice() {
     let random_choice = Math.floor(Math.random() * 3);
-    console.log(random_choice);
 
     // return string based on the random number generated
     switch(random_choice) {
@@ -16,10 +15,58 @@ function getComputerChoice() {
 
 // function to get user input
 function getHumanChoice() {
-    let human_choice = prompt("Enter rock, paper or scissors");
+    let human_choice = prompt("Enter rock, paper or scissors").toLowerCase();
     return human_choice;
 }
 
 // variables to keep track of the score
 let humanScore = 0;
 let computerScore = 0;
+
+// function to play one round
+function playRound(humanChoice, computerChoice) {
+    switch(humanChoice){
+        case "rock":
+            if (computerChoice === "scissors") {
+                console.log("You won! Computer chose scissors\n");
+                humanScore++;
+            } else if (computerChoice === "paper") {
+                console.log("You lost! Computer chose paper\n")
+                computerScore++;
+            } else {
+                console.log("It is a draw!\n")
+            }
+            break;
+
+        case "paper":
+            if (computerChoice === "rock") {
+                console.log("You won! Computer chose rock\n");
+                humanScore++;
+            } else if (computerChoice === "scissors") {
+                console.log("You lost! Computer chose scissors\n")
+                computerScore++;
+            } else {
+                console.log("It is a draw!\n")
+            }
+            break;
+        
+        case "scissors":
+            if (computerChoice === "paper") {
+                console.log("You won! Computer chose paper\n");
+                humanScore++;
+            } else if (computerChoice === "rock") {
+                console.log("You lost! Computer chose rock\n")
+                computerScore++;
+            } else {
+                console.log("It is a draw!\n")
+            }
+            break;
+    } 
+    return;
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
